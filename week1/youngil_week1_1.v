0bcfce5851c2eeced0bf64b97a4e3af48e55fb96
youngilsong <108123401+youngilsong@users.noreply.github.com>
2023-02-18 23:33:22 +0900

    Update youngil_week1_1.v

--- week1/youngil_week1_1.v
+++ week1/youngil_week1_1.v
@@ -16,41 +16,34 @@ localparam STATE_IDLE 				= 4'd0;
 localparam STATE_START 				= 4'd1;
 localparam STATE_DEV_SEL 			= 4'd2;
 localparam STATE_RW				= 4'd3;
-localparam STATE_READ 				= 4'd4;
-localparam STATE_WRITE		 		= 4'd5;
-localparam STATE_ACK			 	= 4'd6;
-localparam STATE_REG_SEL	 		= 4'd7;
-localparam STATE_DATA		 		= 4'd8;
-localparam STATE_NACK		 		= 4'd9;
-localparam STATE_STOP				= 4'd10;
+localparam STATE_ACK_W			 	= 4'd4;
+
+localparam STATE_REG_SEL	 		= 4'd5;
+localparam STATE_ACK_REG		 	= 4'd6;
+
+localparam STATE_READ 				= 4'd7;
+localparam STATE_WRITE		 		= 4'd8;
+
+localparam STATE_ACK_DATA			= 4'd9;
+localparam STATE_NACK		 		= 4'd10;
+localparam STATE_STOP				= 4'd11;
+
 
 reg [3:0]  state = STATE_IDLE;
 reg [3:0]  next_state = STATE_IDLE;
 reg [7:0]  Register = 0;
 reg [6:0]  Device = 0;
-reg 	   ACK = 1;
+reg [3:0]  bit_count = 4'd0;
 
-reg	   SDA = 1'b1;
-reg	   SCL = 1'b1;
+reg	   SDA;
+reg	   SCL;
 
-reg	   START_done = 1'b0;
-reg	   DEV_SEL_done = 1'b0;
-reg	   REG_SEL_done = 1'b0;
-
-
-
-
-reg [31:0] count = 32'b0;
-
-reg [3:0]  bit_count = 3'b0;
+reg [7:0] count;
 
 ////////////////////////////////////////////////////////////////////////////////////
-always @ (posedge SCL) begin
-	SDA = SDA_in + SDA_out;			
-end
-////////////////////////////////////////////////////////////////////////////////////
-always @ (posedge SCL) begin
-	if(~rst)begin
+
+always @ (posedge clk) begin
+	if(!rst)begin
 		state <= STATE_IDLE;
 	end
 
@@ -70,28 +63,57 @@ always @ (*) begin
 		end
 
 		STATE_START : 
-		if (START_done) begin
+		if (count == 8'd29) begin
 			next_state = STATE_DEV_SEL;
 		end
 		else  begin
-			next_state = STATE_START;
+			next_state = STATE_IDLE;
 		end
 
 		STATE_DEV_SEL:
-		 if (DEV_SEL_done) begin
-			next_state = STATE_READ;
+		 if (count == 8'd22 ) begin
+			next_state = STATE_RW;
 		end
 		else begin
 			next_state = STATE_DEV_SEL;
             	end
 		
 		STATE_RW:
-		 if (rst) begin
-			next_state = STATE_ACK;
+		 if (count == 8'd21) begin
+			next_state = STATE_ACK_W;
 		end
 		else begin
-			next_state = STATE_RW;
+			next_state = STATE_IDLE;
             	end
+
+		STATE_ACK_W :  				
+		if ((count == 8'd20) & (SDA_in == 0)) begin
+			next_state = STATE_REG_SEL;
+		end
+		else begin
+			next_state = STATE_IDLE;
+		end
+
+		STATE_REG_SEL : 				
+		if (count == 8'd12) begin
+			next_state = STATE_ACK_REG;
+		end
+		else begin
+			next_state = STATE_REG_SEL;
+		end
+		
+		STATE_ACK_REG : 				
+		if ((count == 8'd11) & (SDA_in == 0)) begin
+			if(RW_sel) begin
+				next_state = STATE_READ;
+			end
+			else begin
+				next_state = STATE_WRITE;
+			end
+		end
+		else begin
+			next_state = STATE_IDLE;
+		end
 
 		STATE_READ :  				
 		if (rst) begin
@@ -103,34 +125,18 @@ always @ (*) begin
 		
 		STATE_WRITE : 				
 		if (rst) begin
-			next_state = STATE_ACK;
+			next_state = STATE_ACK_DATA;
 		end
 		else begin
 			next_state = STATE_WRITE;
 		end
 
-		STATE_ACK : 				
-		if (rst) begin
-			next_state = STATE_REG_SEL;
-		end
-		else begin
-			next_state = STATE_ACK;
-		end
-
-		STATE_REG_SEL : 				
-		if (rst) begin
-			next_state = STATE_DATA;
-		end
-		else begin
-			next_state = STATE_REG_SEL;
-		end
-
-		STATE_DATA : 				
+		STATE_ACK_DATA : 				
 		if (rst) begin
 			next_state = STATE_NACK;
 		end
 		else begin
-			next_state = STATE_DATA;
+			next_state = STATE_ACK_DATA;
 		end
 		
 		STATE_NACK : 				
@@ -141,7 +147,7 @@ always @ (*) begin
 			next_state = STATE_NACK;
 		end
 
-		STATE_STOP : 				
+		STATE_STOP : 		
 		if (rst) begin
 			next_state = STATE_IDLE;
 		end
@@ -149,81 +155,105 @@ always @ (*) begin
 			next_state = STATE_STOP;
 		end
 
+		default next_state = STATE_IDLE;
 	endcase
 end
+
 ////////////////////////////////////////////////////////////////////////////////////
-always @ (posedge SCL) begin
-	case (state)
+
+always @ (posedge clk) begin
+	case (state)  
 		STATE_IDLE : // Initialization
 		begin
 			Register <= Reg_addr;
 			Device <= Dev_addr;
-			ACK <= 1;
-			SCL <= 1'b1;
+
+			SCL_out <= 1'b1;
 			SDA_out <= 1'b1;
-			bit_count <= 3'd1;
+			
+			bit_count <= 4'd0;
 		end
 		
 		STATE_START : // Write Start bit(0) on SDA (From Master to Slave)
 		begin
 			SDA_out <= 1'b0;
-			START_done <= 1'b1;
-			bit_count <= 3'd7;	// set bit_count 7 for Next STATE(STATE_DEV_SEL)
+			bit_count <= 4'd7;
 		end
-
+		
 		STATE_DEV_SEL : // Write Device Address [6:0] on SDA (From Master to Slave)
 		begin
 			if(bit_count) begin
-				if(Dev_addr[bit_count - 1]) begin
+				if(Dev_addr[bit_count - 4'd1]) begin
 					SDA_out <= 1'b1;
 				end
 				else begin
 					SDA_out <= 1'b0;
 				end
-				bit_count <= bit_count - 1'b1;
-				START_done <= 1'b0;
 			end
-			else begin
-				DEV_SEL_done = 1'b1;
-			end
+			bit_count = bit_count - 4'd1;
 		end
 
 		STATE_RW :	//Write RW bit on SDA (From Master to Slave)
 		begin	
-			if(RW_sel & REG_SEL_done) begin
-				SDA_out <= 1'b1;
-			end
-			else  begin
-				SDA_out <= 1'b0;
-			end
+			SDA_out <= 1'b0;
+		end
 
+		STATE_ACK_W :	//Read ARK bit (From Slave to Master)
+		begin	
+			SDA_out <= SDA_in;
+			bit_count <= 4'd8;
 		end
 		
+		STATE_REG_SEL : // Write Register Address [7:0] on SDA (From Master to Slave)
+		begin
+			if(bit_count) begin
+				if(Reg_addr[bit_count - 4'd1]) begin
+					SDA_out <= 1'b1;
+				end
+				else begin
+					SDA_out <= 1'b0;
+				end
+			end
+			bit_count = bit_count - 4'd1;
+		end
+
+		STATE_ACK_REG :	//Read ARK bit (From Slave to Master)
+		begin	
+			SDA_out <= SDA_in;
+
+		end
 	endcase
 end
 ////////////////////////////////////////////////////////////////////////////////////
 
-always @ (posedge clk) begin			// 5 MHz clk , SCL
-	if(!rst) begin	//(rst == 0)
-		count <= 32'b0;
+always @ (posedge clk) begin			
+	if(!rst) begin	// rst Active
+		count <= 8'd31;
 	end
 	
-	else begin	// (rst == 1)
-		if(SDA == 0) begin	// count & SCL Activation, after SDA Low Condition
-			if(count == 9) begin
-				count <= count + 32'b0;
-				SCL <= SCL ^ 1'b1;
-			end
-			else begin
-				count <= count + 32'd1;
-			end
+	else begin
+		if(count > 8'd0) begin
+			count <= count - 8'd1;
 		end
-
 		else begin
-			count <= 32'd0;
+			count <= 8'd31;
 		end
 	end
 end
-
 ////////////////////////////////////////////////////////////////////////////////////
+always @ (posedge clk) begin
+	if ( state > STATE_START ) begin
+		SCL_out <= SCL_out^1'b1;
+	end
+	else begin
+		SCL_out <= 1'b1;
+	end
+end
+////////////////////////////////////////////////////////////////////////////////////
+always @ (posedge clk) begin
+	SDA <= SDA_in + SDA_out;
+	SCL <= SCL_out;
+end 	
+////////////////////////////////////////////////////////////////////////////////////
+
 endmodule
