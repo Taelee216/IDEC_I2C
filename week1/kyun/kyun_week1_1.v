ded1d2f0ebc59cc5b3b834b4abd04ec145d93847
KYUN <taelee12@ajou.ac.kr>
2023-02-20 20:24:08 +0900

    kyun

--- week1/kyun/kyun_week1_1.v
+++ week1/kyun/kyun_week1_1.v
@@ -1,3 +1,18 @@
+`timescale 1ns/1ps
+/*
+Start Condition: The SDA line switches from a high voltage level to a low voltage level before the SCL line switches from high to low.
+
+Stop Condition: The SDA line switches from a low voltage level to a high voltage level after the SCL line switches from low to high.
+
+Address Frame: A 7 or 10 bit sequence unique to each slave that identifies the slave when the master wants to talk to it.
+
+Read/Write Bit: A single bit specifying whether the master is sending data to the slave (low voltage level) 
+or requesting data from it (high voltage level).
+
+ACK/NACK Bit: Each frame in a message is followed by an acknowledge/no-acknowledge bit. 
+If an address frame or data frame was successfully received, an ACK bit is returned to the sender from the receiving device.
+*/
+
 module IDEC_i2c(
     //basic input
     input wire          clk,
