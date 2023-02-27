84cc5d3a1bdd2b78457a60edc5714e8d8549d4b8
youngil <song960615@gmail.com>
2023-02-27 13:44:31 +0900

    asdasd

--- 기본/I2C_Master_TB.v
+++ 기본/I2C_Master_TB.v
@@ -5,9 +5,9 @@ reg [7:0] _Reg_addr;
 reg [6:0] _Dev_addr;
 reg	   	  clk,rst,_RW_sel;
 
-reg	 	 SDA_in;
-wire	 SDA_out;
-wire	 SCL_out;
+reg	 	  SDA_in;
+wire	  SDA_out;
+wire	  SCL_out;
 
 I2C_Master u1 (._Data_in(_Data_in), ._Reg_addr(_Reg_addr), ._Dev_addr(_Dev_addr), .clk(clk), .rst(rst), ._RW_sel(_RW_sel), .SDA_in(SDA_in), .SDA_out(SDA_out), .SCL_out(SCL_out));
 
@@ -17,17 +17,17 @@ initial begin
 end
 								//__________able to change__________//
 initial begin
-rst = 1'b0;
-#9
-rst = 1'b1;
+	rst = 1'b0;
+	#9
+	rst = 1'b1;
 end
 
 initial begin 
-_Dev_addr <= 7'b1010101;
-_RW_sel <= 1'b0;
-_Data_in <=  8'b01010101;
-SDA_in <= 1'b0;
-_Reg_addr <= 8'b10100101;
+	_Dev_addr <= 7'b1010101;
+	_RW_sel <= 1'b0;
+	_Data_in <=  8'b01010101;
+	SDA_in <= 1'b0;
+	_Reg_addr <= 8'b10100101;
 end
 
 endmodule
